9277465cf80a418d027a8df4792945f719a4ded4
Dominic-Wassef <domwassef@gmail.com>
2022-08-01 00:29:17 -0400

    adding

--- goproj6III/main.go
+++ goproj6III/main.go
@@ -1,55 +1,17 @@
 package main
 
-import "fmt"
-
-type person struct {
-	firstName string
-	lastName  string
-	email     string
-	description
-}
-
-type description struct {
-	age     int
-	zipCode int
-	city    string
-}
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
-	person1 := person{
-		firstName: "Jim",
-		lastName:  "Morris",
-		email:     "jimmorris@gmail.com",
-		description: description{
-			age:     32,
-			zipCode: 90210,
-			city:    "LA",
-		},
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
-	person2 := person{
-		firstName: "Marcus",
-		lastName:  "Johnson",
-		email:     "MarcusJ@gmail.com",
-		description: description{
-			age:     63,
-			zipCode: 32601,
-			city:    "Gainesville",
-		},
-	}
-	person1Pointer := &person1
-	person2Pointer := &person2
-	person1Pointer.updateAgeZipcodeCity(66, 34639, "Tampa")
-	person2Pointer.updateAgeZipcodeCity(77, 00000, "N/A")
-	print(person1)
-	print(person2)
-}
-
-func (pointerToPerson *person) updateAgeZipcodeCity(newAge int, newZipcode int, newCity string) {
-	(*pointerToPerson).age = newAge
-	(*pointerToPerson).zipCode = newZipcode
-	(*pointerToPerson).city = newCity
-}
-
-func print(p person) {
-	fmt.Printf("%+v", p)
+	io.Copy(os.Stdout, resp.Body)
 }
