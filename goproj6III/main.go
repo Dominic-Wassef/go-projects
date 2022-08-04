26e089df1c2fba01d7fa5db49308dfe14847eef2
Dominic-Wassef <domwassef@gmail.com>
2022-08-04 04:32:53 -0400

    adding

--- goproj6III/main.go
+++ goproj6III/main.go
@@ -1,17 +1,42 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type squaure struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+func (s squaure) getArea() float64 {
+	tot := s.sideLength * s.sideLength
+	return tot
+}
+
+func (t triangle) getArea() float64 {
+	total := (0.5 * t.base * t.height)
+	return total
+}
+
+func printArea(t triangle, s squaure) {
+	fmt.Println("Square:", s.getArea())
+	fmt.Println("Triangle:", t.getArea())
+}
 
 func main() {
-	resp, err := http.Get("http://google.com")
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
+	tin := triangle{
+		height: 24.393,
+		base:   49.293,
 	}
-	io.Copy(os.Stdout, resp.Body)
+	sin := squaure{
+		sideLength: 32.394,
+	}
+	printArea(tin, sin)
 }
