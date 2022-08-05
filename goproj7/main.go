de484d52be510845b31f606d7e8854f12b6cd7cc
Dominic-Wassef <domwassef@gmail.com>
2022-08-05 03:07:26 -0400

    adding

--- goproj7/main.go
+++ goproj7/main.go
@@ -1,6 +1,28 @@
-// Here we will build a status checker that will make a http.get to multi urls
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	ls := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://amazon.com",
+		"http://hulu.com",
+		"http://stackoverflow.com",
+	}
+	for _, l := range ls {
+		d(l)
+	}
+}
+
+func d(s string) {
+	_, err := http.Get(s)
+	if err != nil {
+		fmt.Println(s + " is down")
+		return
+	}
+	fmt.Println(s + " is up")
 }
