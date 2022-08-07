e5a476f4180eb43eab1cf5a12beef3bd5905fc0e
Dominic-Wassef <domwassef@gmail.com>
2022-08-06 22:48:37 -0400

    adding

---
+++ goproj7II/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+	}
+	for _, link := range links {
+		go checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link + " might be down!")
+		return
+	}
+	fmt.Println(link + " is up!")
+}
